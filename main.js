aa574c93aa19a519a1aae94c0fb650e0fc095143
h-viseria <hviseria@gmail.com>
2026-05-01 12:49:29 +0530

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-import { initTabNavigation } from './app/ui/tabNavigation.js';
-import { initAppController } from './app/ui/appController.js';
-
-const appRootElement = document.getElementById('app-root');
-
-initTabNavigation({ rootElement: appRootElement });
-initAppController();
-
