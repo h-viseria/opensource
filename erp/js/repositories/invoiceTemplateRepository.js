42aa438cbf7854c1bcf3b5a6aa8e8345f952baf5
h-viseria <hviseria@gmail.com>
2026-07-27 09:46:46 +0400

    Sync js/repositories/invoiceTemplateRepository.js

---
+++ erp/js/repositories/invoiceTemplateRepository.js
@@ -0,0 +1,32 @@
+/**
+ * Invoice template repository (Word/ODT binaries).
+ */
+
+import { STORES } from '../core/constants.js';
+import { BaseRepository } from './baseRepository.js';
+import { withTransaction } from '../db/database.js';
+import * as idb from '../db/idb.js';
+
+export class InvoiceTemplateRepository extends BaseRepository {
+  constructor() {
+    super(STORES.INVOICE_TEMPLATES);
+  }
+
+  /** @param {string} bookId */
+  async findByBook(bookId) {
+    const rows = await this.findByIndex('bookId', bookId);
+    return rows.sort((a, b) => String(a.name).localeCompare(String(b.name)));
+  }
+
+  /** @param {string} bookId */
+  async deleteByBook(bookId) {
+    const rows = await this.findByBook(bookId);
+    return withTransaction(this.storeName, 'readwrite', async (tx) => {
+      const store = idb.store(tx, this.storeName);
+      for (const row of rows) await idb.remove(store, row.id);
+      return rows.length;
+    });
+  }
+}
+
+export const invoiceTemplateRepository = new InvoiceTemplateRepository();
