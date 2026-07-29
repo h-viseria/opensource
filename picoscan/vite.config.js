a7785789efdc6cfc6cef0d0987dbb5aa941a39e5
h-viseria <hviseria@gmail.com>
2026-07-29 17:19:23 +0400

    Sync vite.config.js

---
+++ picoscan/vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: './',
+  server: {
+    port: 5173,
+    host: '127.0.0.1',
+  },
+  build: {
+    target: 'es2022',
+    sourcemap: true,
+  },
+  worker: {
+    format: 'es',
+  },
+});
