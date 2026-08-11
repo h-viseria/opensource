55f102e91f4582e7555ee35f228c841340b74677
h-viseria <hviseria@gmail.com>
2026-08-11 14:42:29 +0400

    Sync sw.js

--- erp/sw.js
+++ erp/sw.js
@@ -3,7 +3,7 @@
  * Spec section 20: Offline mode, auto-update detection.
  */
 
-const CACHE_VERSION = 'pe-v0.18.0';
+const CACHE_VERSION = 'pe-v0.18.1';
 const SHELL_CACHE = CACHE_VERSION + '-shell';
 const RUNTIME_CACHE = CACHE_VERSION + '-runtime';
 
