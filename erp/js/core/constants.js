c3c260bf23e87fcdc67ee8559191562157e6232f
h-viseria <hviseria@gmail.com>
2026-08-11 14:42:38 +0400

    Sync js/core/constants.js

--- erp/js/core/constants.js
+++ erp/js/core/constants.js
@@ -3,7 +3,7 @@
  */
 
 export const APP_NAME = 'PicoERP';
-export const APP_VERSION = '0.18.0';
+export const APP_VERSION = '0.18.1';
 
 /** IndexedDB database name. */
 export const DB_NAME = 'erpDataStore';
