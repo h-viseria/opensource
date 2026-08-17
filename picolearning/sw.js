42d19dedf351fe162528769a8aed6142dd71b348
h-viseria <hviseria@gmail.com>
2026-08-17 17:04:09 +0400

    Sync sw.js

---
+++ picolearning/sw.js
@@ -0,0 +1,52 @@
+const CACHE_VERSION = 'pl-v0.1.1';
+const SHELL_CACHE = CACHE_VERSION + '-shell';
+const RUNTIME_CACHE = CACHE_VERSION + '-runtime';
+
+const SHELL_URLS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './css/variables.css',
+  './css/base.css',
+  './css/layout.css',
+  './css/components.css',
+  './css/learning.css',
+  './js/app.js',
+  './icons/icon.svg',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(SHELL_CACHE).then((cache) => cache.addAll(SHELL_URLS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => !k.startsWith(CACHE_VERSION)).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  if (req.method !== 'GET') return;
+  const url = new URL(req.url);
+  if (url.origin !== self.location.origin) return;
+
+  event.respondWith(
+    caches.match(req).then((cached) => {
+      const network = fetch(req)
+        .then((res) => {
+          if (res.ok && (url.pathname.endsWith('.js') || url.pathname.endsWith('.css') || url.pathname.endsWith('.html'))) {
+            const clone = res.clone();
+            caches.open(RUNTIME_CACHE).then((c) => c.put(req, clone));
+          }
+          return res;
+        })
+        .catch(() => cached);
+      return cached || network;
+    })
+  );
+});
